85f82ab8135382898e4c3bb2f057b178821dbf6d
Eric Oligney <EOengineer@gmail.com>
2017-05-13 12:41:00 -0400

    constructed core classes, environment information, and report generation

---
+++ Config.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace CheckLinksConsole
+{
+    public class Config
+    {
+        // constructor
+        public Config(string[] args)
+        {
+            var inMemory = new Dictionary<string, string>
+            {
+                {"site", "https://g0t4.github.io/pluralsight-dotnet-core-xplat-apps" },
+                {"output:folder", "reports1" }
+            };
+            var configBuilder = new ConfigurationBuilder()
+                .AddInMemoryCollection(inMemory)
+                .SetBasePath(Directory.GetCurrentDirectory())
+                // set base path sets the directory where
+                // file configs will be stored, like checksettings.json
+                .AddJsonFile("checksettings.json", true)
+                .AddCommandLine(args)
+                .AddEnvironmentVariables();
+
+            var configuration = configBuilder.Build();
+            Site = configuration["site"];
+            Output = configuration.GetSection("output").Get<OutputSettings>();
+        }
+
+        public string Site { get; set; }
+        public OutputSettings Output { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace CheckLinksConsole
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var config = new Config(args);
+            Directory.CreateDirectory(config.Output.GetReportDirectory()); // could also do configuration.GetSection("output").Get<OutputSettings>(); and this would also skipping creating the new outputsettings object
+            Console.WriteLine($"Saving file to {config.Output.GetReportFilePath()}");            
+            
+            var client = new HttpClient();
+            var body = client.GetStringAsync(config.Site);
+            Console.WriteLine(body.Result);
+
+            Console.WriteLine();
+            Console.WriteLine("Links");
+            var links = LinkChecker.GetLinks(body.Result);
+            links.ToList().ForEach(Console.WriteLine);
+            
+            // write out links
+            // File.WriteAllLines(outputPath, links);
+            var checkedLinks = LinkChecker.CheckLinks(links);
+            using (var file = File.CreateText(config.Output.GetReportFilePath()))
+            // using is like a try block, allows the system to drop the connection if something does wrong
+            foreach (var link in checkedLinks.OrderBy(l => l.IsMissing))
+            {
+                var status = link.IsMissing ? "missing" : "ok";
+
+                file.WriteLine($"{status} - {link.Link}");
+            }
+        }
+    }
+}
